bf9f1832f03310a2cd6e6dab881933a745525615
manishasry <manishayadav1818@gmail.com>
2016-01-13 00:20:33 +0530

    topics and questions

--- src/app/events/events.controller.js
+++ src/app/events/events.controller.js
@@ -1,35 +1,69 @@
 'use strict';
 
 angular.module('bhendi')
-        .controller('EventCtrl', function ($scope, $mdDialog, $log, $mdToast) {
+        .controller('EventCtrl', function ($scope) {
             $scope.events = [];
+            $scope.topics =
+                    [
+                      {title: "Percentages"},
+                      {title: "Averages"},
+                      {title: "Work and Time"},
+                      {title: "Pipes and Cisterns"},
+                      {title: "Profit and Loss"},
+                      {title: "Shares"},
+                      {title: "Simple Interest and Compound Interest"},
+                      {title: "Mixtures and Alligations"},
+                      {title: "Permutations and Combinations"},
+                      {title: "Probability"}
+                    ];
+
+                    $scope.questions =
+                              [
+                                {
+                                  question:'1. 25 * 25 =',
+                                  option1:'625',
+                                  option2:'125',
+                                  option3:'1225',
+                                  answer:'625'
+                                },
+                                {
+                                  question:'2. Acronym for National Aeronautics and Space Administration is',
+                                  option1:'NAASA',
+                                  option2:'NASA',
+                                  option3:'None of the above.',
+                                  answer:'NASA'
+                                },
+                                {
+                                  question:'3. Acrophobia is',
+                                  option1:'Fear of water',
+                                  option2:'Fear of depth',
+                                  option3:'Fear of heights',
+                                  answer:'Fear of heights'
+                                },
+                                {
+                                  question:'4. 11111 + 1111 + 111 + 11 + 1 = ',
+                                  option1:'12345',
+                                  option2:'22222',
+                                  option3:'12221',
+                                  answer:'12345'
+                                },
+                                {
+                                  question:'5. Which of the following is improper fraction? ',
+                                  option1:'45/100',
+                                  option2:'34/5',
+                                  option3:'99/125',
+                                  answer:'34/5'
+                                }
+                              ];
+
+
+
+
+
 
             $scope.data = {
                     group1 : ''
                     }
-            $scope.showAdd = function (ev) {
-
-
-                $mdDialog.show({
-
-                    controller: 'UpdateEventCtrl',
-                    templateUrl: 'app/events/new/newEvent.html',
-                    targetEvent: ev
-                }).then(function (answer) {
-                    $log.debug(JSON.stringify(answer));
-                    $scope.events.push(answer);
-                    $scope.notify('Event Created');
-                }, function () {
-                    $log.info('You cancelled the dialog.');
-                });
-            };
-
-            $scope.deleteEvent=function(item){
-                $scope.events.splice(item,1);
-                $scope.notify('Event Deleted');
-            };
-
-
 
         })
         .config(function ($stateProvider) {
